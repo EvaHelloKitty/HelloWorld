d8d35ead7ffd957a105c7baa5e1b9874e94786c9
eva <eva.v.henderson@gmail.com>
2017-07-08 23:59:08 -0400

    Add cupcake..still works

---
+++ src/CupcakePic.java
@@ -0,0 +1,48 @@
+
+
+
+public class CupcakePic {
+
+	public static void main(String[] args) {
+		myLittlecupcake();
+			
+	}
+
+	public static void myLittlecupcake(){
+
+
+
+		System.out.println("	       777777");                  
+		System.out.println("           77777777");               
+		System.out.println("           77777777");                 
+		System.out.println("        :::77777777::");               
+		System.out.println("     ::::::::7777:::::::::");          
+		System.out.println("    :I:::8::8::::::8:::::::");         
+		System.out.println("   :::::::::::::8::::::::8::~");       
+		System.out.println("::+:::8:::::::::::::::::8::::::::::");    
+		System.out.println(":::::::::::::::::::::::::::::::::::::~");  
+		System.out.println(":::8::::::::::8:::::::::::::::::8::::");  
+		System.out.println("::::::::88:::::::::::7::::::88:::::8::"); 
+		System.out.println("~:::::8:::::::::::::::::::::::::::::::="); 
+		System.out.println("~~~:::::::~~:::::::::::8~:::~=::::::::==");
+		System.out.println("~~~:::::~=~~~~::8::===~:::~~~====~::~==");
+		System.out.println("~~~~~~=====~~~~~~~~=====~~~~~~=========");
+		System.out.println("~~~IZ====~~~~~~~$7=====~~~~~ =====   ");
+		System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");     
+		System.out.println(" ZZZ.ZZZZZZZZZZZZZZZZZZZZZZZZZZ");     
+		System.out.println(" ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");      
+		System.out.println("  ZZZ ZZZZ ZZZZZZZZZZZZZZZZZZZ");      
+		System.out.println("   ZZZZZZZZZZZZZZZZZZZZZZ ZZZ");       
+		System.out.println("  ZZZZZZZZZZZZZZZZZ?ZZZZZZZ");        
+		System.out.println("   ZZ=ZZZZZZZZZZZZZ ZZZ?ZZZ");        
+		System.out.println("   ZZZZZZZ~ZZZZZZZZ ZZZZZZ");         
+		System.out.println("     ZZ ZZZ ZZZZZZZZZZZZZZZ");         
+		System.out.println("     ZZZZZZZZZZZZZZZZZ ZZ");          
+		
+
+
+
+	}	
+}
+
+//System.out.println("I wuv my sista Evas!!!! :D!!!!<3<3<3<3<3<3<3<3<3<3.....and Da Supanatawies!!!!! :D");
